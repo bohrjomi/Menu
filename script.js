5c8cd12f3aeaef4cf104ad9ea59d76e472dbe897
bohrjomi <aqaminerala@mail.ru>
2023-03-20 22:46:01 +0200

    text2

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function run(){
     console.log('run', document.getElementById('img').src)
-    document.getElementById('img').src = 'wold.png'
+    document.getElementById('img').src = 'cattouchret.png'
 }
 function run2(){
     console.log('run2', document.getElementById('img').src)
-    document.getElementById('img').src = 'cattouchret.png'
+    document.getElementById('img').src = 'wold.png'
 }
- function run3() {
+function run3(){
+    console.log('run3', document.getElementById('img').src)
+    document.getElementById('img').src = 'cat.png'
+}
+function run4(){
+    console.log('run3', document.getElementById('img').src)
+    document.getElementById('img').src = 'euro.png'
+}
+ function menu1() {
     const panel = document.getElementById("panel");
     if (panel.style.display === "none") {
 
